d980dc17ff8ec35c950d5e7f2ef424ba92c307be
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2025-12-03 22:08:34 +0100

    modifico diseño de paner de administracion version 1.0

---
+++ backend/resources/lang/en/pagination.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'previous' => '&laquo; Previus',
+    'next' => 'Next &raquo;',
+    'showing' => 'Showing',
+    'to' => 'to',
+    'of' => 'of',
+    'results' => 'results',
+];
--- backend/resources/views/admin/usuarios/index.blade.php
+++ backend/resources/views/admin/usuarios/index.blade.php
@@ -1,32 +1,40 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-white dark:text-gray-200 leading-tight">
             {{ __('usuario.index_title') }}
         </h2>
     </x-slot>
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        {{-- INICIO BUSCADOR Y BOTÓN CREAR --}}
-        <div class="flex justify-between items-center mb-4">
+{{-- INICIO BUSCADOR Y BOTÓN CREAR --}}
+<div class="flex justify-between items-center mb-4 gap-2 pr-4">
 
-            {{-- Formulario de Búsqueda --}}
-            <form method="GET" action="{{ route('admin.usuarios.index') }}" class="flex items-center w-full max-w-sm">
-                <x-text-input type="text" name="search"
-                    :placeholder="__('usuario.search_placeholder_user')"
-                    class="w-full mr-2" value="{{ request('search') }}" />
-                <x-primary-button type="submit">
-                    {{ __('usuario.button_search') }}
-                </x-primary-button>
-            </form>
+    {{-- Formulario de Búsqueda --}}
+    <form method="GET" action="{{ route('admin.usuarios.index') }}" class="flex items-center w-full max-w-sm">
+        <x-text-input type="text" name="search"
+            :placeholder="__('usuario.search_placeholder_user')"
+            class="w-full mr-2" value="{{ request('search') }}" />
+        <x-primary-button type="submit">
+            {{ __('usuario.button_search') }}
+        </x-primary-button>
+    </form>
 
-            {{-- Botón de Creación --}}
-            <a href="{{ route('admin.usuarios.create') }}">
-                <x-primary-button>
-                    {{ __('usuario.button_create_new') }}
-                </x-primary-button>
-            </a>
-        </div>
-        {{-- FIN BUSCADOR Y BOTÓN CREAR --}}
+    {{-- Botón de Creación --}}
+    <a href="{{ route('admin.usuarios.create') }}">
+        <x-primary-button class="flex items-center justify-center h-10">
+            {{-- Versión MÓVIL: Icono (Usuario +) --}}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 md:hidden">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 019.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+            </svg>
+
+            {{-- Versión PC: Texto completo --}}
+            <span class="hidden md:block">
+                {{ __('usuario.button_create_new') }}
+            </span>
+        </x-primary-button>
+    </a>
+</div>
+{{-- FIN BUSCADOR Y BOTÓN CREAR --}}
             @if (session('status'))
                 <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400 p-3 bg-green-100 dark:bg-green-700 rounded-lg">
                     {{ session('status') }}
