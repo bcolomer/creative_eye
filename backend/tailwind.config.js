852c22c15201c7178d534f60f3a5d15228e9b369
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2025-12-04 20:03:22 +0100

    se modifica estilo del front de back

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,18 +1,28 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Poppins", "Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            //  COLORES CORPORATIVOS
+            colors: {
+                brand: {
+                    teal: "#00747C" /* Teal Principal */,
+                    light: "#00BBC9" /* Teal Claro (Acentos) */,
+                    graylight: "#CACACA" /* Gris Claro */,
+                    graymed: "#878787" /* Gris Medio */,
+                    graydark: "#202022" /* Gris Oscuro */,
+                },
             },
         },
     },
