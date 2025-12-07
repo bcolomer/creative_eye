5116d9acf3af6a9d9b0d98366e2469a3de801811
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2025-12-07 17:56:41 +0100

    Configurar CI/CD y dominios Sanctum

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,creative-eye.duckdns.org,52.200.92.250',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
