ea72c00fbf615c8ba1a8c66e2d3adb5d337d2f6b
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2025-12-09 17:13:45 +0100

    añado enlaces a tienda y a panel en menu hamburguesa

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -22,7 +22,7 @@ Route::middleware('guest')->group(function () {
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+    /*     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
@@ -32,7 +32,7 @@ Route::middleware('guest')->group(function () {
         ->name('password.reset');
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+        ->name('password.store'); */
 });
 
 Route::middleware('auth')->group(function () {
