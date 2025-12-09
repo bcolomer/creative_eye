88f4e18c5f97a74c6a76d0e462576d365afe5ebb
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2025-12-09 17:24:07 +0100

    corrijo clave para el texto de informacion al borrar producto

--- backend/resources/lang/en/admin.php
+++ backend/resources/lang/en/admin.php
@@ -7,4 +7,7 @@ return [
     'delete_account_info' => 'Deleting this account will permanently remove all associated orders and data (including the profile picture).',
     'confirm_delete_user' => 'Are you sure you want to delete the account of :nombre? This change is permanent.',
     'usuario.button_delete_account' => 'Delete Account',
+    'delete_product_info' => 'Once this product is deleted, all associated data will also be permanently removed. Please confirm that you want to delete this product.',
+
+
 ];
